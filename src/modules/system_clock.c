2a1615df8504ff56d266df546fb20088849a2dbb
Tjalling7 <tjalling1995@hotmail.com>
2015-10-14 08:40:50 +0200

    Changed RTC freq
    RTC freq changed

--- src/modules/system_clock.c
+++ src/modules/system_clock.c
@@ -13,21 +13,21 @@ static int time_remaining;
 static int mscounter = 0;
 
 INTERRUPT_FUNCTION(rtc_irq); 
-
+bool lowtime = false;
 void sysclock_init(void)
 {
 	Disable_global_interrupt();
 	
 	INTC_register_interrupt(&rtc_irq, AVR32_RTC_IRQ, AVR32_INTC_INT0);
 
-	if (!rtc_init(&AVR32_RTC, RTC_OSC_32KHZ, RTC_PSEL_32KHZ_1HZ))
+	if (!rtc_init(&AVR32_RTC, RTC_OSC_32KHZ, 0))
 	{
 		//usart_write_line(&AVR32_USART0, "Error initializing the RTC\r\n");
 		while(1);
 	}
 
-	// Set top value to 0 to generate an interrupt every seconds
-	rtc_set_top_value(&AVR32_RTC, 0);
+	// Set top value to 0 to generate an interrupt every tick
+	rtc_set_top_value(&AVR32_RTC, 15);
 	// Enable the interrupts
 	rtc_enable_interrupt(&AVR32_RTC);
 	// Enable the RTC
@@ -39,23 +39,20 @@ void sysclock_init(void)
 static void rtc_irq()
 {
 	rtc_clear_interrupt(&AVR32_RTC);
-	rtc_int = 1;
+	rtc_int ++;
+	if(rtc_int >= 1)
+	{
+		LED_Toggle(LED1);
+		if ((&AVR32_RTC)->top == 15)
+			(&AVR32_RTC)->top = 16;
+		else
+			(&AVR32_RTC)->top = 15;
+		rtc_int = 0;
+	}
 }
 
 int rtcSec()
 {
-	if(rtc_int == 1)
-	{
-		mscounter++;
-		if(mscounter == 5)
-		{
-			LED_Toggle(LED1);
-			mscounter = 0;
-			return 1;
-		}
-		rtc_int = 0;
-	}
-	return 0;
 }
 
 
