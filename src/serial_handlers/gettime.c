b98ff95d7666a7939767e7cf1a58a174b07fb3ba
mooshbeef <bramkamies@gmail.com>
2016-01-11 12:06:54 +0100

    Improved gettime and settime commands

--- src/serial_handlers/gettime.c
+++ src/serial_handlers/gettime.c
@@ -8,8 +8,8 @@
 
 void serial_handler_gettime(char identifier, char type, char* body, int body_length)
 {
-	char responseBody[4] = {0};
 	uint64_t millis = datetime_get_milliseconds();
+	char responseBody[sizeof(millis)] = {0};
 	memcpy(responseBody, &millis, sizeof(millis));
-	serial_send_packet(identifier, type, responseBody, 4);
+	serial_send_packet(identifier, type, responseBody, sizeof(millis));
 }
--- src/serial_handlers/settime.c
+++ src/serial_handlers/settime.c
@@ -8,8 +8,17 @@
 
 void serial_handler_settime(char identifier, char type, char* body, int body_length)
 {
+	char response[1] = {0};
 	uint64_t millis = 0;
-	memcpy(&millis, body, sizeof(millis));
-	datetime_set_time(millis);
-	serial_send_packet(identifier, type, NULL, 0);
+	if (body_length == sizeof(millis))
+	{
+		memcpy(&millis, body, sizeof(millis));
+		datetime_set_time(millis);
+		response[0] = 1;
+	}
+	else
+	{
+		response[0] = 0;
+	}
+	serial_send_packet(identifier, type, response, 1);
 }
